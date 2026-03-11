85db068da9b21e34779200f71ceabb85d815586a
pragath17MA <pragathannadurai03122005@gmail.com>
2026-03-11 19:58:41 +0530

    problem

---
+++ valid anagram.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char ch1[]=s.toCharArray();
+        char ch2[]=t.toCharArray();
+        if(ch1.length!=ch2.length)return false;
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        for(int i=0;i<ch1.length;i++)
+        {
+            if(ch1[i]!=ch2[i])
+            {
+                return false;
+            } 
+        }
+        return true;
+
+    }
+}
